d0f277ae746e72621363dc11e1931c5879a443df
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-05-07 18:39:12 +0300

    feat: add styles for Button components, add @types/styled-componets

--- project-management-app/src/components/Button/Button.tsx
+++ project-management-app/src/components/Button/Button.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import styled from 'styled-components';
+const StyledButton = styled.button`
+border: none;
+padding: 10px 15px;
+font-size: 18px;
+cursor: pointer;
+&: hover {
+  outline: none;
+}`
 type ButtonProps = {
   children: string;
 }
 
 function Button (props: ButtonProps) {
-  return <button>{props.children}</button>
+  return <StyledButton>{props.children}</StyledButton>
 }
 
 export default Button;
