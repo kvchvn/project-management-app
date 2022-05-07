62dcec2ab3d72b4cc7e22875310758d8486c6b32
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-05-07 19:19:30 +0300

    feat: change some button styles

--- project-management-app/src/components/Button/Button.tsx
+++ project-management-app/src/components/Button/Button.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import styled from 'styled-components';
+
 const StyledButton = styled.button`
+min-width: 100px;
 border: none;
+border-radius: 5px;
 padding: 10px 15px;
+background: #4676D7;
 font-size: 18px;
 cursor: pointer;
 &: hover {
+  background: #1d49aa;
+}
+&: focus {
   outline: none;
+  box-shadow: 0 0 0 4px #cbd6ee;
 }`
 type ButtonProps = {
   children: string;
