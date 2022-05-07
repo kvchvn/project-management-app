d0d0a009b9a8bd5b347e46084d91377cb62b4295
Anton Kachan <aa.kvchvn@gmail.com>
2022-05-07 13:34:09 +0300

    feat: create requests for get all boards and create new board

--- project-management-app/src/layouts/containers.ts
+++ project-management-app/src/layouts/containers.ts
@@ -1,9 +1,5 @@
 import styled from 'styled-components';
 
-<<<<<<< HEAD
 export const StyledWrapper = styled.div`
-=======
-export const Wrapper = styled.div`
->>>>>>> 0b870af (init: start to create main page)
   padding: 20px;
 `;
