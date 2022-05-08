030ed1f8c005026bc31303babd67f7e96bd3b3a8
evitla <s.dinmuhamed@gmail.com>
2022-05-08 16:26:56 +0600

    feat: add useSignOut custom hook

---
+++ project-management-app/src/hooks/use-sign-out.ts
@@ -0,0 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+import { routerPaths } from '../constants/common-constants';
+import { removeFromLocalStorage } from '../utils/common';
+
+const useSignOut = () => {
+  const navigate = useNavigate();
+
+  const signOut = () => {
+    removeFromLocalStorage('token');
+    navigate(`/${routerPaths.welcome}`);
+  };
+
+  return signOut;
+};
+
+export default useSignOut;
