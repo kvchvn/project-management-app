133ef5b3a76d1f0ee62df6e4479900b7947895b4
evitla <s.dinmuhamed@gmail.com>
2022-05-10 11:54:32 +0600

    refactor: add auth form validation schemas

---
+++ project-management-app/src/components/AuthForm/validation-schemas.ts
@@ -0,0 +1,14 @@
+import * as yup from 'yup';
+import { MIN_PASSWORD_LENGTH, NAME_VALIDATOR } from '../../constants/common-constants';
+
+export const signInValidationSchema = yup.object({
+  login: yup.string().required(),
+  password: yup
+    .string()
+    .min(MIN_PASSWORD_LENGTH, `Must be at least ${MIN_PASSWORD_LENGTH} characters`)
+    .required(),
+});
+
+export const signUpValidationSchema = signInValidationSchema.shape({
+  name: yup.string().matches(NAME_VALIDATOR, 'Please, enter a valid name').required(),
+});
