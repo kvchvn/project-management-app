3ee2e04cbc5b19ae0563f5e0ac54ef9d00483c04
evitla <s.dinmuhamed@gmail.com>
2022-05-10 12:51:03 +0600

    refactor: add local storage keys constant

--- project-management-app/src/constants/common-constants.ts
+++ project-management-app/src/constants/common-constants.ts
@@ -8,5 +8,10 @@ export const routerPaths = {
   default: '*',
 };
 
+export const LOCAL_STORAGE_KEYS = {
+  user: 'user',
+  token: 'token',
+};
+
 export const NAME_VALIDATOR = /^[a-zA-Z -]{2,30}$/;
 export const MIN_PASSWORD_LENGTH = 6;
--- project-management-app/src/utils/common.ts
+++ project-management-app/src/utils/common.ts
@@ -1,15 +1,18 @@
+import { LOCAL_STORAGE_KEYS } from '../constants/common-constants';
+
 export const setToLocalStorage = <T>(
-  key: string,
+  key: keyof typeof LOCAL_STORAGE_KEYS,
   value: T,
   serializer: (value: T) => string = JSON.stringify
 ) => localStorage.setItem(key, serializer(value));
 
 export const getFromLocalStorage = <T>(
-  key: string,
+  key: keyof typeof LOCAL_STORAGE_KEYS,
   deserializer: (value: string) => T = JSON.parse
 ) => {
   const value = localStorage.getItem(key);
   return value ? deserializer(value) : undefined;
 };
 
-export const removeFromLocalStorage = (key: string) => localStorage.removeItem(key);
+export const removeFromLocalStorage = (key: keyof typeof LOCAL_STORAGE_KEYS) =>
+  localStorage.removeItem(key);
