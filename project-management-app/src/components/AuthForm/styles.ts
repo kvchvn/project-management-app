116045f992d6d924cead26e0104564e174460e2f
evitla <s.dinmuhamed@gmail.com>
2022-05-10 11:55:16 +0600

    feat: add AuthForm component with basic styles

---
+++ project-management-app/src/components/AuthForm/styles.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const StyledForm = styled.form`
+  width: 400px;
+  margin: 0 100px;
+`;
+
+export const StyledInputContainer = styled.div`
+  width: 100%;
+  margin: 15px 0;
+  position: relative;
+  font-size: 16px;
+
+  input {
+    border: none;
+    background-color: #e9ecef;
+    outline: none;
+    width: 100%;
+    padding: 0.75em;
+
+    &:focus ~ label,
+    &:not(:placeholder-shown) ~ label {
+      opacity: 1;
+      transform: translate(calc(-100% - 20px), -50%);
+
+      &:after {
+        content: ':';
+      }
+    }
+  }
+
+  label {
+    position: absolute;
+    pointer-events: none;
+    top: 50%;
+    left: 0.75em;
+    transform-origin: 100% 50%;
+    transform: translateY(-50%);
+    opacity: 0.5;
+    transition: transform 0.2s, color 0.2s;
+  }
+
+  span {
+    position: absolute;
+    right: calc(-100% - 5px);
+    top: 50%;
+    width: 100%;
+    transform: translateY(-50%);
+
+    &::first-letter {
+      text-transform: uppercase;
+    }
+  }
+`;
