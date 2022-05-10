b70b9b2f1fe51545295eb979c2a69c38b9a1f890
evitla <s.dinmuhamed@gmail.com>
2022-05-10 13:03:24 +0600

    feat: navigate to main page if user is already authorized

--- project-management-app/src/pages/Authorization/Authorization.tsx
+++ project-management-app/src/pages/Authorization/Authorization.tsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 import AuthForm from '../../components/AuthForm';
 import { useSignIn, useSignUp } from '../../hooks';
+import { routerPaths } from '../../constants/common-constants';
 import { UnauthorizedUser } from '../../interfaces/user';
+import { TStore } from '../../store';
 
 function Authorization() {
+  const navigate = useNavigate();
+
   const [isSignUpForm, setIsSignUpForm] = useState(true);
+
+  const { user } = useSelector((store: TStore) => store.userReducer);
+
+  useEffect(() => {
+    if (user) navigate(routerPaths.main);
+  }, [user, navigate]);
 
   const signUp = useSignUp();
   const signIn = useSignIn();
