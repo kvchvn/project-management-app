c69fb302b4d0d7917c303440321416245f8ae928
evitla <s.dinmuhamed@gmail.com>
2022-05-11 12:54:28 +0600

    feat: keep token inside user object

--- project-management-app/src/store/slices/user.ts
+++ project-management-app/src/store/slices/user.ts
@@ -2,32 +2,24 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AuthorizedUser } from '../../interfaces/user';
 import { getFromLocalStorage } from '../../utils/common';
 
-const token = getFromLocalStorage<string>('token');
-
-const initialState: { user?: Omit<AuthorizedUser, 'token'>; token?: string } = {
-  user: token ? getFromLocalStorage('user') : undefined,
-  token,
+const initialState: { user?: AuthorizedUser } = {
+  user: getFromLocalStorage('user'),
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    onSignUp: (state, { payload }: PayloadAction<Omit<AuthorizedUser, 'token'>>) => {
+    onSignIn: (state, { payload }: PayloadAction<AuthorizedUser>) => {
       state.user = payload;
-    },
-
-    onSignIn: (state, { payload }: PayloadAction<{ token: string }>) => {
-      state.token = payload.token;
     },
 
     onSignOut: (state) => {
       state.user = undefined;
-      state.token = undefined;
     },
   },
 });
 
 export const userReducer = userSlice.reducer;
 
-export const { onSignIn, onSignOut, onSignUp } = userSlice.actions;
+export const { onSignIn, onSignOut } = userSlice.actions;
