945fadc1b1859ccb2c60595b74ac42205c31443f
Anton Kachan <aa.kvchvn@gmail.com>
2022-05-12 12:28:48 +0300

    feat: create basic header layout without toggler

--- project-management-app/src/components/Header/Header.tsx
+++ project-management-app/src/components/Header/Header.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { routerPaths } from '../../constants/common-constants';
+import { StyledWrapper } from '../../layouts/containers';
+import { StyledHeader, Nav, NavButton } from './styles';
 
 function Header() {
-  return <p>It is the Header</p>;
+  return (
+    <StyledHeader>
+      <StyledWrapper>
+        <h1>AppName</h1>
+        <Nav>
+          <NavButton as={Link} to={routerPaths.profile}>
+            Edit profile
+          </NavButton>
+          <NavButton>Create board</NavButton>
+          <NavButton>Log out</NavButton>
+        </Nav>
+      </StyledWrapper>
+    </StyledHeader>
+  );
 }
 
 export default Header;
---
+++ project-management-app/src/components/Header/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.header`
+  background-color: lightgray;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  gap: 20px;
+  border: 1px solid red;
+`;
+
+export const NavButton = styled.button`
+  border: 2px solid black;
+  background-color: white;
+  width: 70px;
+  height: 30pxs;
+  text-align: center;
+`;
