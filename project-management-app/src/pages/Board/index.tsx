eeec3dc54b264ee602f6c0a43cf0146b7ae49376
evitla <s.dinmuhamed@gmail.com>
2022-05-15 12:36:43 +0600

    feat: render Columns in Board page

--- project-management-app/src/pages/Board/index.tsx
+++ project-management-app/src/pages/Board/index.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+import { MOCKED_BOARD } from '../../constants/api';
+import { useColumnsQuery, useCreateColumn } from '../../hooks';
+import { TStore } from '../../store';
+
+import ColumnsContainer from '../../components/ColumnsContainer';
 
 function Board() {
-  return <h2>Board page</h2>;
+  const { user } = useSelector((store: TStore) => store.userReducer);
+  const { id: boardId } = MOCKED_BOARD;
+
+  const { data: columns, ...columnsQueryResult } = useColumnsQuery({
+    token: user?.token,
+    boardId,
+  });
+  const createColumn = useCreateColumn({ token: user?.token, boardId });
+
+  const handleNewColumnCreate = async (title: string) => {
+    if (columnsQueryResult.isSuccess && columns) {
+      await createColumn.mutateAsync({ title, order: columns.length + 1 });
+    }
+  };
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <h2>Board page</h2>
+      {columnsQueryResult.isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        columns && <ColumnsContainer columns={columns} onNewColumnCreate={handleNewColumnCreate} />
+      )}
+    </DndProvider>
+  );
 }
 
 export default Board;
