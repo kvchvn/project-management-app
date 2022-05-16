e0674317879dbc8dc646df807100cf32ccd3736d
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-05-16 23:27:33 +0300

    feat: add component ErrorMessage, add error.gif

---
+++ project-management-app/src/components/ErrorBoundary/ErrorMessage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import img from './error.gif';
+import styled from "styled-components";
+
+const StyledError = styled.img`
+display: block;
+width: 250px;
+height: 250px;
+objectFit: contain;
+margin: 0 auto;
+`
+const ErrorMessage = () => {
+  return (
+    <StyledError src={img} />
+  )
+}
+
+export default ErrorMessage;
