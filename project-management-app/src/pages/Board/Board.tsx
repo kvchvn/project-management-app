ad2ff71ea861b70fe26cfdb08890ee315a7bede7
evitla <s.dinmuhamed@gmail.com>
2022-05-17 12:43:59 +0600

    refactor: get user and boardId inside hooks

--- project-management-app/src/pages/Board/Board.tsx
+++ project-management-app/src/pages/Board/Board.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
-import { MOCKED_BOARD } from '../../constants/api';
-import { useColumnsQuery, useCreateColumn } from '../../hooks';
-import { TStore } from '../../store';
+import { useColumnsQuery, useColumnMutation } from '../../hooks';
 
 import ColumnsContainer from '../../components/ColumnsContainer';
 
 function Board() {
-  const { user } = useSelector((store: TStore) => store.userReducer);
-  const { id: boardId } = MOCKED_BOARD;
+  const { data: columns, ...columnsQueryResult } = useColumnsQuery();
 
-  const { data: columns, ...columnsQueryResult } = useColumnsQuery({
-    token: user?.token,
-    boardId,
-  });
-  const createColumn = useCreateColumn({ token: user?.token, boardId });
+  const { mutateAsync: create } = useColumnMutation({ method: 'create' });
 
   const handleNewColumnCreate = async (title: string) => {
     if (columnsQueryResult.isSuccess && columns) {
-      await createColumn.mutateAsync({ title, order: columns.length + 1 });
+      await create({ title, order: columns.length + 1 });
     }
   };
 
