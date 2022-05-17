e1b1cf59927858ae6f2853bb4e09a87f833b9591
evitla <s.dinmuhamed@gmail.com>
2022-05-17 13:55:03 +0600

    feat: implement the ability to change column title

--- project-management-app/src/components/Column/index.tsx
+++ project-management-app/src/components/Column/index.tsx
@@ -1,21 +1,55 @@
-import React, { memo } from 'react';
+import React, { memo, useRef, useState } from 'react';
+import { TITLE_ROWS } from '../../constants/common-constants';
 
-import { useColumnDragAndDrop } from '../../hooks';
+import { useClickOutside, useColumnDragAndDrop, useColumnMutation } from '../../hooks';
 import { Column as IColumn } from '../../interfaces/column';
 
-import { StyledColumn } from './styles';
+import { StyledColumn, StyledTitle } from './styles';
 
 interface ColumnProps extends IColumn {
   moveColumn: (id: string, to: number) => void;
   findColumn: (id: string) => { index: number };
 }
 
-function Column({ id, title, moveColumn, findColumn }: ColumnProps) {
+function Column({ id, title, order, moveColumn, findColumn }: ColumnProps) {
+  const titleRef = useRef<HTMLDivElement>(null);
+  const [isEditingTitle, setIsEditingTitle] = useState(false);
   const { isDragging, drag, drop } = useColumnDragAndDrop({ id, moveColumn, findColumn });
+  const [newTitle, setNewTitle] = useState(title);
+
+  const { mutateAsync: update } = useColumnMutation({ method: 'update', columnId: id });
+
+  useClickOutside(titleRef, () => {
+    setIsEditingTitle(false);
+    setNewTitle(title);
+  });
+
+  const handleTitleEdit = () => setIsEditingTitle(true);
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNewTitle(e.target.value);
+  };
+
+  const handleTitleSubmit = async () => {
+    setIsEditingTitle(false);
+    await update({ title: newTitle, order });
+  };
+
+  const handleTitleEditCancel = () => setNewTitle(title);
 
   return (
-    <StyledColumn ref={(node) => drag(drop(node))} isDragging={isDragging}>
-      <h3>{title}</h3>
+    <StyledColumn ref={(node) => !isEditingTitle && drag(drop(node))} isDragging={isDragging}>
+      <StyledTitle ref={titleRef} isEditing={isEditingTitle}>
+        <h2>{title}</h2>
+        <textarea
+          rows={TITLE_ROWS}
+          value={newTitle}
+          onChange={handleTitleChange}
+          onClick={handleTitleEdit}
+        />
+        <button onClick={handleTitleSubmit}>√</button>
+        <button onClick={handleTitleEditCancel}>x</button>
+      </StyledTitle>
     </StyledColumn>
   );
 }
