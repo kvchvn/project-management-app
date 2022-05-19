b004e84abb17816a5c6ba1d2f454b0ef171517ba
Anton Kachan <aa.kvchvn@gmail.com>
2022-05-19 16:52:02 +0300

    feat: make delete-profile button

--- project-management-app/src/components/ProfileForm/index.tsx
+++ project-management-app/src/components/ProfileForm/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import { AuthorizedUser } from '../../interfaces/user';
-import { StyledForm, StyledInputContainer } from './styles';
+import {
+  StyledButtonDelete,
+  StyledContainer,
+  StyledDangerBox,
+  StyledForm,
+  StyledInputContainer,
+} from './styles';
 import { useUserSelector } from '../../store/slices/user';
 import validationSchema from './validationSchema';
 
@@ -16,32 +22,48 @@ function ProfileForm() {
   };
 
   const initialValues = user || defaultUser;
-  const { handleSubmit, handleChange, values, errors } = useFormik({
+  const { handleSubmit, handleChange, values, errors, touched } = useFormik({
     initialValues,
     validationSchema,
     onSubmit: (values) => alert(values.name + '__' + values.login),
   });
 
   return (
-    <StyledForm onSubmit={handleSubmit}>
-      <StyledInputContainer>
-        <input id="name" name="name" value={values.name} onChange={handleChange} placeholder=" " />
-        <label htmlFor="name">Name</label>
-        {errors ? <span>{errors.name}</span> : null}
-      </StyledInputContainer>
-      <StyledInputContainer>
-        <input
-          id="login"
-          name="login"
-          value={values.login}
-          onChange={handleChange}
-          placeholder=" "
-        />
-        <label htmlFor="login">Login</label>
-        {errors ? <span>{errors.login}</span> : null}
-      </StyledInputContainer>
-      <button type="submit">Update form</button>
-    </StyledForm>
+    <StyledContainer>
+      <StyledForm onSubmit={handleSubmit}>
+        <p>You can change the fields below.</p>
+        <StyledInputContainer>
+          <input
+            id="name"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+            placeholder=" "
+          />
+          <label htmlFor="name">Name</label>
+          {touched.name && errors.name ? <span>{errors.name}</span> : null}
+        </StyledInputContainer>
+        <StyledInputContainer>
+          <input
+            id="login"
+            name="login"
+            value={values.login}
+            onChange={handleChange}
+            placeholder=" "
+          />
+          <label htmlFor="login">Login</label>
+          {touched.login && errors.login ? <span>{errors.login}</span> : null}
+        </StyledInputContainer>
+        <button type="submit">Update form</button>
+      </StyledForm>
+      <StyledDangerBox>
+        <p>
+          This button will remove your profile forever.
+          <strong> Be carefully.</strong>
+        </p>
+        <StyledButtonDelete type="button">Delete profile</StyledButtonDelete>
+      </StyledDangerBox>
+    </StyledContainer>
   );
 }
 
--- project-management-app/src/components/ProfileForm/styles.ts
+++ project-management-app/src/components/ProfileForm/styles.ts
@@ -1,10 +1,35 @@
 import styled from 'styled-components';
 import { StyledInputContainer } from '../AuthForm/styles';
 
-const StyledForm = styled.form`
+export const StyledContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
   margin: 0 auto;
   width: 60vw;
   max-width: 600px;
 `;
 
-export { StyledInputContainer, StyledForm };
+export const StyledForm = styled.form`
+  width: 100%;
+`;
+
+export const StyledDangerBox = styled.div`
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
+  border: 2px solid darkred;
+  border-radius: 10px;
+  font-size: 0.9rem;
+`;
+
+export const StyledButtonDelete = styled.button`
+  width: 150px;
+  height: 40px;
+  border: 2px solid darkred;
+  border-radius: 5px;
+`;
+
+export { StyledInputContainer };
