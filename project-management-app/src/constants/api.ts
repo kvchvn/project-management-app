46020986d4f7576e6298d95c166a1c779cf1c88a
evitla <s.dinmuhamed@gmail.com>
2022-05-20 11:54:03 +0600

    feat: add useCreateTask custom hook

--- project-management-app/src/constants/api.ts
+++ project-management-app/src/constants/api.ts
@@ -16,6 +16,7 @@ export const QUERY_KEYS = {
   users: 'users',
   columns: 'columns',
   allBoards: 'all-boards',
+  tasks: 'tasks',
 };
 
 const USER: AuthorizedUser | undefined = getFromLocalStorage('user');
