483ac1aef21ffebb3f35aa5ddaacfac9ee653cfd
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-05-22 00:33:12 +0300

    feat: add img and styles for 404 page

--- project-management-app/src/pages/NotFound/index.tsx
+++ project-management-app/src/pages/NotFound/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Styled404, StyledImg } from './style';
 
 function NotFound() {
-  return <h2>NotFound page</h2>;
+  return (
+    <Styled404>
+      <StyledImg src="./404.jpg" alt="not found" />
+      <Link to="/">Go to Main page</Link>
+    </Styled404>
+  );
 }
 
 export default NotFound;
