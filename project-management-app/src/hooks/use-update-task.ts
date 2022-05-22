384a27a21c87ea48af08807de77e553d3117e75e
evitla <s.dinmuhamed@gmail.com>
2022-05-22 11:54:52 +0600

    feat: create useUpdateTask custom hook

---
+++ project-management-app/src/hooks/use-update-task.ts
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
+import { useSelector } from 'react-redux';
+import { QUERY_KEYS, URLS } from '../constants/api';
+import { TaskDetailed } from '../interfaces/task';
+import { TStore } from '../store';
+import { update } from '../utils/api';
+
+const useUpdateTask = ({ columnId }: { columnId: string }) => {
+  const [taskId, setTaskId] = useState('');
+  const { user } = useSelector((store: TStore) => store.userReducer);
+
+  const queryClient = useQueryClient();
+
+  const updateTask = useMutation(
+    async (task: TaskDetailed) => {
+      if (user) {
+        const { id: taskId, ...updatedTask } = task;
+        setTaskId(taskId);
+        const url = `${URLS.boards}/${task.boardId}/columns/${columnId}/tasks/${taskId}`;
+        const config = { headers: { Authorization: `Bearer ${user.token}` } };
+
+        return update<Omit<TaskDetailed, 'id'>, TaskDetailed>(url, updatedTask, config);
+      }
+    },
+    {
+      onSuccess: () => queryClient.refetchQueries(`${QUERY_KEYS.tasks}/${taskId}`),
+    }
+  );
+
+  return updateTask;
+};
+
+export default useUpdateTask;
