b0901aee0ff20afb7fb49a6fbb845539712f2a7a
evitla <s.dinmuhamed@gmail.com>
2022-05-22 11:56:22 +0600

    refactor: rename useDragAndDrop hook

--- project-management-app/src/hooks/index.ts
+++ project-management-app/src/hooks/index.ts
@@ -1,9 +1,9 @@
 import useAuthorization from './use-authorization';
 import useClickOutside from './use-click-outside';
-import useColumnDragAndDrop from './use-column-drag-n-drop';
 import useColumnsQuery from './use-columns-query';
 import useCreateColumn from './use-create-column';
 import useCreateTask from './use-create-task';
+import useDragAndDrop from './use-drag-n-drop';
 import useForm from './use-form';
 import useRemoveColumn from './use-remove-column';
 import useSignIn from './use-sign-in';
@@ -19,10 +19,10 @@ import useRemovingBoard from './use-removing-board';
 export {
   useAuthorization,
   useClickOutside,
-  useColumnDragAndDrop,
   useColumnsQuery,
   useCreateColumn,
   useCreateTask,
+  useDragAndDrop,
   useForm,
   useRemoveColumn,
   useSignIn,
--- project-management-app/src/hooks/use-drag-n-drop.ts
+++ project-management-app/src/hooks/use-drag-n-drop.ts
@@ -41,10 +41,18 @@ const useColumnDragAndDrop = ({ id }: { id: string }) => {
 
   const [, drop] = useDrop(
     () => ({
+<<<<<<< HEAD:project-management-app/src/hooks/use-column-drag-n-drop.ts
       accept: DND_ITEM_TYPES.column,
       hover: (dragItem: { id: string }) => {
         if (dragItem.id !== id) {
           dispatch(onMoveColumn({ dragId: dragItem.id, hoverId: id }));
+=======
+      accept: itemType,
+      hover: ({ id: draggedId }: { id: string }) => {
+        if (draggedId !== id) {
+          const { index: overIndex } = findColumn(id);
+          moveColumn(draggedId, overIndex);
+>>>>>>> 124bfea (refactor: rename useDragAndDrop hook):project-management-app/src/hooks/use-drag-n-drop.ts
         }
       },
     }),
@@ -54,4 +62,4 @@ const useColumnDragAndDrop = ({ id }: { id: string }) => {
   return { isDragging, drag, dragPreview, drop };
 };
 
-export default useColumnDragAndDrop;
+export default useDragAndDrop;
