76f0eeb121954edbd72601addccc5e1f2ae007ed
evitla <s.dinmuhamed@gmail.com>
2022-05-23 16:22:50 +0600

    refactor: add useMoveItems custom hook

--- project-management-app/src/components/ColumnsContainer/index.tsx
+++ project-management-app/src/components/ColumnsContainer/index.tsx
@@ -1,60 +1,26 @@
-import React, { memo, useCallback, useEffect, useState } from 'react';
+import React, { memo } from 'react';
 import { useDrop } from 'react-dnd';
 
 import Column from '../Column';
 import ColumnCreator from '../ColumnCreator';
-import { useUpdateColumn } from '../../hooks';
+import { useMoveItems, useUpdateColumn } from '../../hooks';
 import { Column as IColumn } from '../../interfaces/column';
 import { DND_ITEM_TYPES } from '../../constants/common';
-import { calculateUpdatedOrder } from '../../utils/common';
 
 import { StyledColumnsContainer } from './styles';
 
 function ColumnsContainer({ items }: { items: IColumn[] }) {
-  const [columns, setColumns] = useState(items);
-
   const { mutateAsync: update } = useUpdateColumn();
 
-  useEffect(() => setColumns(items), [items]);
-
-  const findColumn = useCallback(
-    (id: string) => {
-      const index = columns.findIndex((c) => c.id === id);
-      return { column: columns[index], index };
-    },
-    [columns]
-  );
-
-  const moveColumn = useCallback(
-    (droppedId: string, hoverIndex: number) => {
-      const droppedIndex = findColumn(droppedId).index;
-      const columnsCopy = [...columns];
-      const droppedColumn = columnsCopy.splice(droppedIndex, 1)[0];
-      columnsCopy.splice(hoverIndex, 0, droppedColumn);
-
-      setColumns(columnsCopy);
-    },
-    [findColumn, columns, setColumns]
-  );
-
-  const updateColumnOrder = useCallback(
-    async (droppedId: string, hoverIndex: number) => {
-      const { index: droppedIndex, column: droppedColumn } = findColumn(droppedId);
-      if (droppedIndex === hoverIndex) return;
-
-      const direction = droppedIndex > hoverIndex ? 'forward' : 'backward';
-      const hoverColumn = columns[hoverIndex];
-      const nextToDroppedColumn =
-        columns[droppedIndex > hoverIndex ? droppedIndex + 1 : droppedIndex - 1];
-
-      await update({
-        id: droppedId,
-        title: droppedColumn.title,
-        order: calculateUpdatedOrder(hoverColumn.order, nextToDroppedColumn?.order, direction),
-      });
-    },
-    [findColumn, columns, update]
-  );
+  const {
+    items: columns,
+    findItem,
+    moveItem,
+    updateItemOrder,
+  } = useMoveItems({
+    items,
+    update,
+  });
 
   const [, drop] = useDrop(() => ({ accept: DND_ITEM_TYPES.column }));
 
@@ -64,9 +30,9 @@ function ColumnsContainer({ items }: { items: IColumn[] }) {
         <Column
           key={column.id}
           {...column}
-          moveColumn={moveColumn}
-          findColumn={findColumn}
-          updateColumn={updateColumnOrder}
+          moveColumn={moveItem}
+          findColumn={findItem}
+          updateColumn={updateItemOrder}
         />
       ))}
       <ColumnCreator lastColumnOrder={columns[columns.length - 1]?.order} />
--- project-management-app/src/hooks/index.ts
+++ project-management-app/src/hooks/index.ts
@@ -5,10 +5,12 @@ import useCreateColumn from './use-create-column';
 import useCreateTask from './use-create-task';
 import useDragAndDrop from './use-drag-n-drop';
 import useForm from './use-form';
+import useMoveItems from './use-move-items';
 import useRemoveColumn from './use-remove-column';
 import useSignIn from './use-sign-in';
 import useSignOut from './use-sign-out';
 import useSignUp from './use-sign-up';
+import useSortByOrder from './use-sort-by-order';
 import useTasksQuery from './use-tasks-query';
 import useUpdateColumn from './use-update-column';
 import useUpdateTask from './use-update-task';
@@ -23,10 +25,12 @@ export {
   useCreateTask,
   useDragAndDrop,
   useForm,
+  useMoveItems,
   useRemoveColumn,
   useSignIn,
   useSignOut,
   useSignUp,
+  useSortByOrder,
   useTasksQuery,
   useUpdateColumn,
   useUpdateTask,
