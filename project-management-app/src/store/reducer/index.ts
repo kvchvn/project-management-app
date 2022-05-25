ca27fee3bc5a544600a6e47ad35f5bca3e456357
Anton Kachan <aa.kvchvn@gmail.com>
2022-05-26 01:55:17 +0300

    fix: remove unneded langSlice from redux

--- project-management-app/src/store/reducer/index.ts
+++ project-management-app/src/store/reducer/index.ts
@@ -1,6 +1,5 @@
 import { columnReducer } from '../slices/column';
 import { taskReducer } from '../slices/task';
 import { userReducer } from '../slices/user';
-import { langReducer } from '../slices/lang';
 
-export default { columnReducer, langReducer, taskReducer, userReducer };
+export default { columnReducer, taskReducer, userReducer };
