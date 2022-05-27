54cbe5805f10cdb9e418786854b48ada454f9770
evitla <s.dinmuhamed@gmail.com>
2022-05-27 08:22:01 +0600

    refactor: get tasks from store

--- project-management-app/src/components/TasksContainer/index.tsx
+++ project-management-app/src/components/TasksContainer/index.tsx
@@ -1,41 +1,24 @@
 import React, { memo } from 'react';
-import { useDrop } from 'react-dnd';
+import { useSelector } from 'react-redux';
 
 import TaskCreator from '../TaskCreator';
-import { useMoveItems, useUpdateTask } from '../../hooks';
-import { DND_ITEM_TYPES } from '../../constants/common-constants';
-import { TaskDetailed } from '../../interfaces/task';
 import TaskCard from '../TaskCard';
 import { StyledTasksContainer } from './styles';
+import { TStore } from '../../store';
 
-function TasksContainer({ items, columnId }: { items: TaskDetailed[]; columnId: string }) {
-  const { mutateAsync: update } = useUpdateTask({ columnId });
-
-  const {
-    items: tasks,
-    findItem,
-    moveItem,
-    updateItemOrder,
-  } = useMoveItems({
-    items,
-    update,
-  });
-
-  const [, drop] = useDrop(() => ({ accept: DND_ITEM_TYPES.task }));
+function TasksContainer({ columnId }: { columnId: string }) {
+  const { tasksByColumns } = useSelector((store: TStore) => store.taskReducer);
+  const tasks = tasksByColumns[columnId];
 
   return (
-    <StyledTasksContainer ref={drop}>
-      {tasks.map((task) => (
-        <TaskCard
-          key={task.id}
-          {...task}
-          moveTask={moveItem}
-          findTask={findItem}
-          updateTask={updateItemOrder}
-        />
-      ))}
-      <TaskCreator columnId={columnId} lastTaskOrder={tasks[tasks.length - 1]?.order} />
-    </StyledTasksContainer>
+    tasks && (
+      <StyledTasksContainer>
+        {tasks.map((task) => (
+          <TaskCard key={task.id} {...task} />
+        ))}
+        <TaskCreator columnId={columnId} lastTaskOrder={tasks[tasks.length - 1]?.order} />
+      </StyledTasksContainer>
+    )
   );
 }
 
