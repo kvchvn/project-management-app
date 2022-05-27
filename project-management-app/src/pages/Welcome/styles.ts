74cb9e9c1225c35973fe4da91bc40cd664f7acd9
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-05-28 00:55:01 +0300

    refactor: add mediaquery to welcome page and navbar

--- project-management-app/src/pages/Welcome/styles.ts
+++ project-management-app/src/pages/Welcome/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { device } from '../../constants/common';
 import { baseTheme } from '../../styles/theme';
 
 export const StyledAbout = styled.div`
@@ -11,6 +12,10 @@ export const StyledAbout = styled.div`
   border: 3px solid ${baseTheme.colors.bgNavbar};
   margin: 0 auto;
   padding: 10px 0px;
+  @media ${device.mobile} {
+    font-size: 12px;
+    width: 20rem;
+  }
 `;
 export const StyledWrapper = styled.div`
   max-width: 100%;
@@ -22,8 +27,14 @@ export const StyledWrapper = styled.div`
   position: relative;
   align-items: center;
   justify-content: space-between;
+  @media ${device.mobile} {
+    padding: 5px 0;
+  }
 `;
 export const StyledDescription = styled.p`
   padding: 30px 0;
   font-weight: 500;
+  @media ${device.mobile} {
+    padding: 5px 0;
+  }
 `;
