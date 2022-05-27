db8a70addf6284c27381054909ac8ab3bf8a8fa4
evitla <s.dinmuhamed@gmail.com>
2022-05-27 08:19:50 +0600

    refactor: get columns from store

--- project-management-app/src/components/ColumnsContainer/index.tsx
+++ project-management-app/src/components/ColumnsContainer/index.tsx
@@ -3,37 +3,21 @@ import { useDrop } from 'react-dnd';
 
 import Column from '../Column';
 import ColumnCreator from '../ColumnCreator';
-import { useMoveItems, useUpdateColumn } from '../../hooks';
-import { Column as IColumn } from '../../interfaces/column';
 import { DND_ITEM_TYPES } from '../../constants/common';
 
 import { StyledColumnsContainer } from './styles';
+import { useSelector } from 'react-redux';
+import { TStore } from '../../store';
 
-function ColumnsContainer({ items }: { items: IColumn[] }) {
-  const { mutateAsync: update } = useUpdateColumn();
-
-  const {
-    items: columns,
-    findItem,
-    moveItem,
-    updateItemOrder,
-  } = useMoveItems({
-    items,
-    update,
-  });
+function ColumnsContainer() {
+  const { columns } = useSelector((store: TStore) => store.columnReducer);
 
   const [, drop] = useDrop(() => ({ accept: DND_ITEM_TYPES.column }));
 
   return (
     <StyledColumnsContainer ref={drop}>
       {columns.map((column) => (
-        <Column
-          key={column.id}
-          {...column}
-          moveColumn={moveItem}
-          findColumn={findItem}
-          updateColumn={updateItemOrder}
-        />
+        <Column key={column.id} {...column} />
       ))}
       <ColumnCreator lastColumnOrder={columns[columns.length - 1]?.order} />
     </StyledColumnsContainer>
