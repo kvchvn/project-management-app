f7e58e1ae244a3341667ef32fe1929cb0a06eae5
evitla <s.dinmuhamed@gmail.com>
2022-05-27 20:01:27 +0600

    fix: remove Board.tsx

--- project-management-app/src/pages/Board/index.tsx
+++ project-management-app/src/pages/Board/index.tsx
@@ -1,29 +1,19 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import { useColumnsQuery } from '../../hooks';
 
 import ColumnsContainer from '../../components/ColumnsContainer';
-import { Column } from '../../interfaces/column';
 import Loading from '../../components/Loading';
 
 function Board() {
-  const [columns, setColumns] = useState<Column[]>([]);
-  const { data, ...columnsQueryResult } = useColumnsQuery();
-
-  const sortColumns = useCallback(() => {
-    if (data) setColumns(data.sort((a, b) => a.order - b.order));
-  }, [data]);
-
-  useEffect(() => {
-    sortColumns();
-  }, [sortColumns]);
+  const { isLoading } = useColumnsQuery();
 
   return (
     <DndProvider backend={HTML5Backend}>
       <h2>Board page</h2>
-      {columnsQueryResult.isLoading ? <Loading /> : columns && <ColumnsContainer items={columns} />}
+      {isLoading ? <Loading /> : <ColumnsContainer />}
     </DndProvider>
   );
 }
