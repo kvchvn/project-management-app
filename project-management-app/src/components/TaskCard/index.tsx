b82dca8bcd68252e25496c2c2c12b112a116288a
evitla <s.dinmuhamed@gmail.com>
2022-05-27 08:22:14 +0600

    refactor: use useTaskDragAndDrop hook

--- project-management-app/src/components/TaskCard/index.tsx
+++ project-management-app/src/components/TaskCard/index.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { DND_ITEM_TYPES } from '../../constants/common-constants';
-import { useDragAndDrop } from '../../hooks';
+import { useTaskDragAndDrop } from '../../hooks';
 import { TaskDetailed } from '../../interfaces/task';
 
 import { StyledTaskCard } from './styles';
 
-interface TaskCardProps extends TaskDetailed {
-  moveTask: (id: string, to: number) => void;
-  findTask: (id: string) => { index: number };
-  updateTask: (id: string, to: number) => void;
-}
-
-function TaskCard({ id, title, moveTask, findTask, updateTask }: TaskCardProps) {
-  const { isDragging, drag, drop } = useDragAndDrop({
-    id,
-    itemType: DND_ITEM_TYPES.task,
-    moveColumn: moveTask,
-    findColumn: findTask,
-    updateColumn: updateTask,
-  });
+function TaskCard({ id, title, columnId }: TaskDetailed) {
+  const { isDragging, drag, drop } = useTaskDragAndDrop({ id, columnId });
 
   return (
     <StyledTaskCard ref={(node) => drag(drop(node))} isDragging={isDragging}>
