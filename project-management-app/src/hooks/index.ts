a607203999f13e9bb702b212395d8866114cd336
evitla <s.dinmuhamed@gmail.com>
2022-05-27 08:22:40 +0600

    refactor: remove unnecessary custom hooks

--- project-management-app/src/hooks/index.ts
+++ project-management-app/src/hooks/index.ts
@@ -1,16 +1,15 @@
 import useAuthorization from './use-authorization';
 import useClickOutside from './use-click-outside';
+import useColumnDragAndDrop from './use-column-drag-n-drop';
 import useColumnsQuery from './use-columns-query';
 import useCreateColumn from './use-create-column';
 import useCreateTask from './use-create-task';
-import useDragAndDrop from './use-drag-n-drop';
 import useForm from './use-form';
-import useMoveItems from './use-move-items';
 import useRemoveColumn from './use-remove-column';
 import useSignIn from './use-sign-in';
 import useSignOut from './use-sign-out';
 import useSignUp from './use-sign-up';
-import useSortByOrder from './use-sort-by-order';
+import useTaskDragAndDrop from './use-task-drag-n-drop';
 import useTasksQuery from './use-tasks-query';
 import useUpdateColumn from './use-update-column';
 import useUpdateTask from './use-update-task';
@@ -20,17 +19,16 @@ import useRemovingBoard from './use-removing-board';
 export {
   useAuthorization,
   useClickOutside,
+  useColumnDragAndDrop,
   useColumnsQuery,
   useCreateColumn,
   useCreateTask,
-  useDragAndDrop,
   useForm,
-  useMoveItems,
   useRemoveColumn,
   useSignIn,
   useSignOut,
   useSignUp,
-  useSortByOrder,
+  useTaskDragAndDrop,
   useTasksQuery,
   useUpdateColumn,
   useUpdateTask,
