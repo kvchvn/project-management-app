01cc53d33aa39ad6e743fd679e720db04609a00c
Anton Kachan <aa.kvchvn@gmail.com>
2022-05-28 18:46:21 +0300

    feat: implement updating task

--- project-management-app/src/components/Task/index.tsx
+++ project-management-app/src/components/Task/index.tsx
@@ -1,18 +1,55 @@
 import React from 'react';
+import { useFormik } from 'formik';
+
+import { useUpdateTask } from '../../hooks';
+import { TaskDetailed } from '../../interfaces/task';
 import { useTaskSelector } from '../../store/selectors';
-import { StyledContainer, StyledInput } from './styles';
+import {
+  StyledForm,
+  StyledInput,
+  StyledTextarea,
+  StyledButtonsContainer,
+  StyledButton,
+  StyledButtonSubmit,
+} from './styles';
 
 function Task() {
-  const { tasksByColumns: allColumns, taskModal } = useTaskSelector();
-  const { taskId, columnId } = taskModal;
+  const { taskModal, tasksByColumns: allColumns } = useTaskSelector();
+  const { taskId, ...task } = taskModal;
+  const columnId = task.columnId ? task.columnId : '';
+
+  const updateTask = useUpdateTask({ columnId });
   const column = columnId ? allColumns[columnId] : null;
   const taskData = column ? column.find((task) => task.id === taskId) : null;
+
+  const initialValues = {
+    title: taskData ? taskData.title : '',
+    description: taskData ? taskData.description : '',
+  };
+
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues,
+    onSubmit: async ({ title, description }) => {
+      const updatedTaskData = { ...(taskData as TaskDetailed), title, description };
+      updateTask.mutateAsync(updatedTaskData);
+    },
+  });
+
   if (taskData) {
     return (
-      <StyledContainer>
-        <StyledInput defaultValue={taskData.title} />
-        <p>{taskData.description}</p>
-      </StyledContainer>
+      <StyledForm onSubmit={handleSubmit}>
+        <StyledInput id="title" name="title" value={values.title} onChange={handleChange} />
+        <StyledTextarea
+          id="description"
+          name="description"
+          value={values.description}
+          onChange={handleChange}
+        />
+        <StyledButtonsContainer>
+          <StyledButton>Delete</StyledButton>
+          <StyledButtonSubmit>Update</StyledButtonSubmit>
+        </StyledButtonsContainer>
+      </StyledForm>
     );
   }
   return null;
--- project-management-app/src/components/Task/styles.ts
+++ project-management-app/src/components/Task/styles.ts
@@ -1,10 +1,62 @@
 import styled from 'styled-components';
 
-export const StyledContainer = styled.div`
+export const StyledForm = styled.form`
   border: 1px solid red;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  width: 90vw;
+  max-width: 400px;
+  height: 40vh;
+  max-height: 300px;
 `;
 
 export const StyledInput = styled.input`
+  padding: 10px;
+  width: 140px;
+  height: 40px;
+  border: none;
+  text-align: center;
+  border-radius: 10px;
+  transition: background-color 0.25s;
+
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+export const StyledTextarea = styled.textarea`
+  padding: 10px;
+  width: 70%;
+  height: 100px;
+  text-align: center;
+  border: none;
+  border-radius: 10px;
+  resize: none;
+  cursor: pointer;
+  transition: background-color 0.25s;
+
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+export const StyledButtonsContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+export const StyledButton = styled.button`
   width: 100px;
   height: 40px;
+  border-radius: 5px;
+  border: 2px solid gray;
+`;
+
+export const StyledButtonSubmit = styled(StyledButton).attrs({
+  type: 'submit',
+})`
+  background-color: gray;
 `;
