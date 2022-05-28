fe8a4435fb75d5d04ca34f2fa682d8409f946fd6
evitla <s.dinmuhamed@gmail.com>
2022-05-28 21:08:59 +0600

    feat: use ConfirmationModal for boards deletion

--- project-management-app/src/components/BoardCard/index.tsx
+++ project-management-app/src/components/BoardCard/index.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { routerPaths } from '../../constants/common';
 import useRemovingBoard from '../../hooks/use-removing-board';
 import { Board } from '../../interfaces/board';
+import ConfirmationModal from '../ConfirmationModal';
 import { StyledListItem } from './styles';
 
 interface BoardCardProps extends Board {}
 
 function BoardCard({ id, title }: BoardCardProps) {
-  const removeBoard = useRemovingBoard();
+  const [isGoingToRemove, setIsGoingToRemove] = useState(false);
 
-  const deleteBoard = async () => {
-    await removeBoard.mutateAsync(id);
-  };
+  const { mutateAsync: removeBoard } = useRemovingBoard();
+
+  const handleDeleteColumn = () => setIsGoingToRemove(true);
+
+  const handleConfirmDeletion = () => removeBoard(id);
 
   return (
     <StyledListItem>
-      <button onClick={deleteBoard}>x</button>
+      <button onClick={handleDeleteColumn}>x</button>
       <Link to={`${routerPaths.board}${id}/${title}`}>
         <h4>{title}</h4>
       </Link>
+      {isGoingToRemove && (
+        <ConfirmationModal onConfirm={handleConfirmDeletion} setIsOpen={setIsGoingToRemove}>
+          <p>Do you really want to delete the board?</p>
+          <p>You will lose all the lists on the board</p>
+        </ConfirmationModal>
+      )}
     </StyledListItem>
   );
 }
