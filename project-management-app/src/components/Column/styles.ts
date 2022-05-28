f79a2d79df92af6f5c108618f6ecccc2a8b49afa
evitla <s.dinmuhamed@gmail.com>
2022-05-28 21:07:08 +0600

    refactor: remove unnecessary styled component

--- project-management-app/src/components/Column/styles.ts
+++ project-management-app/src/components/Column/styles.ts
@@ -24,22 +24,3 @@ export const StyledColumnHeader = styled.div`
     height: fit-content;
   }
 `;
-
-export const StyledConfirmationModal = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 300px;
-  padding: 10px;
-  border-radius: 6px;
-  text-align: center;
-  background-color: ${({ theme }) => theme.colors.bg};
-
-  div {
-    display: flex;
-    justify-content: space-between;
-    width: 30%;
-    margin: 10px auto 0;
-  }
-`;
