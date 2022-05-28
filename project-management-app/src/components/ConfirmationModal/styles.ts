95ccc47f953244fba34136765b692aa6f701f960
evitla <s.dinmuhamed@gmail.com>
2022-05-28 21:07:59 +0600

    feat: create ConfirmationModal

---
+++ project-management-app/src/components/ConfirmationModal/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledButtonsContainer = styled.div`
+  display: flex;
+  column-gap: 10px;
+  justify-content: space-between;
+  width: 100px;
+  margin: 10px auto 0;
+`;
