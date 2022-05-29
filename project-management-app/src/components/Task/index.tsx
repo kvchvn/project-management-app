f76ee4fa6023886ed63f21049e56fbc1990f700c
Anton Kachan <aa.kvchvn@gmail.com>
2022-05-29 12:25:08 +0300

    feat: implement removing task

--- project-management-app/src/components/Task/index.tsx
+++ project-management-app/src/components/Task/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-import { useUpdateTask } from '../../hooks';
+import { useUpdateTask, useRemoveTask } from '../../hooks';
 import { TaskDetailed } from '../../interfaces/task';
 import { useTaskSelector } from '../../store/selectors';
+import { onCloseTaskModal } from '../../store/slices/task';
 import {
   StyledForm,
   StyledInput,
@@ -14,11 +17,15 @@ import {
 } from './styles';
 
 function Task() {
+  const { id: boardId } = useParams();
   const { taskModal, tasksByColumns: allColumns } = useTaskSelector();
   const { taskId, ...task } = taskModal;
-  const columnId = task.columnId ? task.columnId : '';
+  const dispatch = useDispatch();
 
+  const columnId = task.columnId ? task.columnId : '';
   const updateTask = useUpdateTask({ columnId });
+  const removeTask = useRemoveTask({ boardId, columnId });
+
   const column = columnId ? allColumns[columnId] : null;
   const taskData = column ? column.find((task) => task.id === taskId) : null;
 
@@ -35,21 +42,30 @@ function Task() {
     },
   });
 
-  if (taskData) {
+  const handleRemoveTask = () => {
+    if (taskId) {
+      removeTask.mutateAsync(taskId as string);
+      dispatch(onCloseTaskModal());
+    }
+  };
+
+  if (taskData && task.isOpen) {
     return (
-      <StyledForm onSubmit={handleSubmit}>
-        <StyledInput id="title" name="title" value={values.title} onChange={handleChange} />
-        <StyledTextarea
-          id="description"
-          name="description"
-          value={values.description}
-          onChange={handleChange}
-        />
-        <StyledButtonsContainer>
-          <StyledButton>Delete</StyledButton>
-          <StyledButtonSubmit>Update</StyledButtonSubmit>
-        </StyledButtonsContainer>
-      </StyledForm>
+      <div>
+        <StyledForm onSubmit={handleSubmit} onReset={handleRemoveTask}>
+          <StyledInput id="title" name="title" value={values.title} onChange={handleChange} />
+          <StyledTextarea
+            id="description"
+            name="description"
+            value={values.description}
+            onChange={handleChange}
+          />
+          <StyledButtonsContainer>
+            <StyledButton type="reset">Delete</StyledButton>
+            <StyledButtonSubmit type="submit">Update</StyledButtonSubmit>
+          </StyledButtonsContainer>
+        </StyledForm>
+      </div>
     );
   }
   return null;
