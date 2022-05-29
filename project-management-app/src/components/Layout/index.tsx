ad4a89178b080056295572110f00523861f5da46
Anton Kachan <aa.kvchvn@gmail.com>
2022-05-29 16:55:22 +0300

    feat: add StyledMain in Layout component

--- project-management-app/src/components/Layout/index.tsx
+++ project-management-app/src/components/Layout/index.tsx
@@ -3,10 +3,7 @@ import { matchPath, Outlet, useLocation } from 'react-router-dom';
 import { routerPaths } from '../../constants/common';
 import Footer from '../Footer';
 import Header from '../Header';
-<<<<<<< HEAD
 import StyledMain from '../../styles/components/StyledMain';
-=======
->>>>>>> 29e3438 (feat: create StyledWrapper and wrap with it Outlet)
 import StyledWrapper from '../../styles/components/StyledWrapper';
 
 function Layout() {
