4d509a0efdc2816b5465664fd4d8f0034b2c9ee5
Anton Kachan <aa.kvchvn@gmail.com>
2022-05-29 21:01:18 +0300

    fix: fix styles of StyledMain, StyledWrapper

--- project-management-app/src/styles/components/StyledWrapper.ts
+++ project-management-app/src/styles/components/StyledWrapper.ts
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
+import { device } from '../../constants/common';
 
 const StyledWrapper = styled.div`
   margin: 0 auto;
   border: 1px solid red;
-  width: calc(100% - 20px);
+  width: calc(100% - 200px);
   max-width: 1400px;
+
+  @media ${device.LAPTOP} {
+    width: calc(100% - 100px);
+  }
+
+  @media ${device.TABLET} {
+    width: calc(100% - 20px);
+  }
 `;
 
 export default StyledWrapper;
