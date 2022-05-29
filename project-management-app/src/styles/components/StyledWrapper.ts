a1407da8c90a3cbdda145cb34bd73d33641431d9
Anton Kachan <aa.kvchvn@gmail.com>
2022-05-29 16:53:37 +0300

    feat: add styles for StyledWrapper

--- project-management-app/src/styles/components/StyledWrapper.ts
+++ project-management-app/src/styles/components/StyledWrapper.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
-  padding: 20px;
+  margin: 0 auto;
+  border: 1px solid red;
+  width: calc(100% - 200px);
+  max-width: 1400px;
 `;
 
 export default StyledWrapper;
