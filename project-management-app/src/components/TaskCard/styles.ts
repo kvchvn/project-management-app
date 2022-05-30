e7922f542a4ca41325488188a98b934c9538e82f
evitla <s.dinmuhamed@gmail.com>
2022-05-30 10:37:33 +0600

    refactor: remove comment

--- project-management-app/src/components/TaskCard/styles.ts
+++ project-management-app/src/components/TaskCard/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const StyledTaskCard = styled.div<{ isDragging: boolean }>`
   height: 30px;
-  // TODO: colors need to be set later
   border: 1px solid ${({ isDragging }) => (isDragging ? 'grey' : 'black')};
   margin-bottom: 4px;
   background-color: ${({ isDragging }) => (isDragging ? 'grey' : 'inherit')};
