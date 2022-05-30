9f23e02f5fd5994bdeb681d3d15b2302cdae2330
Anton Kachan <aa.kvchvn@gmail.com>
2022-05-30 19:32:05 +0300

    fix: a little change present styles of Header, Main page

--- project-management-app/src/styles/components/StyledPageTitle.ts
+++ project-management-app/src/styles/components/StyledPageTitle.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const StyledPageTitle = styled.h2`
   font-family: ${({ theme }) => theme.fontFamily.title};
   letter-spacing: 1px;
-  margin-bottom: 20px;
+  margin: 20px 0;
 `;
 
 export default StyledPageTitle;
