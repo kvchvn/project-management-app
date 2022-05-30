964b1aafbe78b1bb1453fa1da58e981ff06ef4bb
evitla <s.dinmuhamed@gmail.com>
2022-05-31 01:57:30 +0600

    fix: app bg color

--- project-management-app/src/styles/global.ts
+++ project-management-app/src/styles/global.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
   html {
     font-family: ${({ theme }) => theme.fontFamily.primary};
-    background-color: ${({ theme }) => theme.colors.bg};
+    background-color: ${({ theme }) => theme.colors.bg.primary};
     color: ${({ theme }) => theme.colors.font};
   }
 `;
